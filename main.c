a3396e174788f0566c586cb7edd19e16d4522781
Amedeo Di Gaetano <alessio.picciafuoco@gmail.com>
2015-05-18 18:37:53 +0200

    commit

--- main.c
+++ main.c
@@ -1,8 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define N 3
+ struct s_ricetta {
+        char nome [20];
+        char descrizione [50];
+        int pomodoro;
+        int mozzarella;
+        int glutine;
+       
+    };
+    
+    typedef struct s_ricetta ricetta;
+    
+    void bubbleSort (int i[], int tanti);
+    
 int main(int argc, char** argv) {
+        
+  int i;    
+  ricetta v;   
+for (i=0;i<N;i++) {
+    printf("Inserisci il nome della ricetta: ");
+    scanf("%s",v.nome);
+    printf ("Inserisci la descrizione della ricetta: ");
+    scanf("%s",v.descrizione);
+    printf("Inserisci la quantita di pomodoro in gramm: ");
+    scanf("%d",&v.pomodoro);
+    printf("Inserisci la quantita di mozzarella in grammi: ");
+    scanf ("%d",&v.mozzarella);
+    printf("Con glutine(inserisca il numero 0 persi insersci qualsisi numero per no");
+    scanf ("%d",& v.glutine);
+   ;
+                    }
+  
+  bubbleSort(int i,N);
+  
+if (v.glutine) {
+    
+
+ FILE *puntaFile;
+puntaFile = fopen("nomi.txt","w+"); 
+  for (i=0;i<N;i++) {
+      scanf("%s",v.nome);
+      scanf("%s",v.descrizione);
+      scanf("%d",&v.pomodoro);
+      scanf("%d",&v.mozzarella);
+      scanf("%d",&v.glutine);
+      
+      
+      
+  }
+fclose(puntaFile);
+              }
+else {
+    FILE *puntaFile;
+puntaFile = fopen("nomi.txt","w+"); 
+  for (i=0;i<N;i++) {
+      scanf("%s",v.nome);
+      scanf("%s",v.descrizione);
+      scanf("%d",&v.pomodoro);
+      scanf("%d",&v.mozzarella);
+      scanf("%d",&v.glutine);
+      
+      
+      
+  }
+fclose(puntaFile);
+}
+
+  
+
+  
+  
+  
+            
+    
+    
+    
+    
+    
+    
 
     return (EXIT_SUCCESS);
 }
+  
+void bubbleSort( int i[],int tanti) 
+{
+    
+    ricetta v;
+    int  x,y,temp;
+    for (x=0;x < tanti-1;x++){
+       for(y=0;x <tanti-1;y++)
+       if(strcmp  > v.nome){
+           temp = v.nome[y];
+           v.nome[y] = v.nome[y+1]; 
+           v.nome[y+1] = temp;
+                   
+                   
+       }
+   
+    
+     }
+}    
+
 
--- nbproject/Package-Debug.bash
+++ nbproject/Package-Debug.bash
@@ -6,16 +6,16 @@
 
 # Macros
 TOP=`pwd`
-CND_PLATFORM=Cygwin-Windows
+CND_PLATFORM=Cygwin_4.x-Windows
 CND_CONF=Debug
 CND_DISTDIR=dist
 CND_BUILDDIR=build
 CND_DLIB_EXT=dll
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/ver20150518_bromantica
-OUTPUT_BASENAME=ver20150518_bromantica
-PACKAGE_TOP_DIR=ver20150518bromantica/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/verb
+OUTPUT_BASENAME=verb
+PACKAGE_TOP_DIR=verb/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/ver20150518bromantica/bin"
+makeDirectory "${NBTMPDIR}/verb/bin"
 copyFileToTmpDir "${OUTPUT_PATH}.exe" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}.exe" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/ver20150518bromantica.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/verb.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/ver20150518bromantica.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/verb.tar *
 checkReturnCode
 
 # Cleanup
